51dbc36c8d2a4dbf7f70f0be29a43ec7f87d74d0
danyal-deekoders <deekoders@gmail.com>
2023-05-24 12:40:13 +0500

    Update ProductCategoryDropdown.js

--- client/src/components/shop/home/ProductCategoryDropdown.js
+++ client/src/components/shop/home/ProductCategoryDropdown.js
@@ -221,22 +221,6 @@ const FilterList = () => {
                 labelledBy={""}
               />
             </div>
-            <div onClick={(e) => closeFilterBar()} className="cursor-pointer">
-              <svg
-                className="w-8 h-8 text-gray-700 hover:bg-gray-200 rounded-full p-1"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
-            </div>
           </div>
         </div>
         <div className="w-2/3 mx-2">
@@ -253,22 +237,6 @@ const FilterList = () => {
                 hasSelectAll={false}
                 labelledBy={""}
               />
-            </div>
-            <div onClick={(e) => closeFilterBar()} className="cursor-pointer">
-              <svg
-                className="w-8 h-8 text-gray-700 hover:bg-gray-200 rounded-full p-1"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
             </div>
           </div>
         </div>
