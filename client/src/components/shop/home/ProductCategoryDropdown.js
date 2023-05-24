3a253e3a518afd359edb1c104396c467cdba215a
danyal-deekoders <deekoders@gmail.com>
2023-05-24 09:59:29 +0500

    design changed

--- client/src/components/shop/home/ProductCategoryDropdown.js
+++ client/src/components/shop/home/ProductCategoryDropdown.js
@@ -127,74 +127,62 @@ const FilterList = () => {
   return (
     <div className={`${data.filterListDropdown ? "" : "hidden"} my-4`}>
       <hr />
-      <div className="w-full flex flex-col">
-        <div className="font-medium py-2">Filter by price</div>
-        <div className="flex justify-between items-center">
-          <div className="flex flex-col space-y-2  w-2/3 lg:w-2/4">
-            <label htmlFor="points" className="text-sm">
-              Price (between 0 and 10$):{" "}
-              <span className="font-semibold text-yellow-700">{range}.00$</span>{" "}
-            </label>
-            <input
-              value={range}
-              className="slider"
-              type="range"
-              id="points"
-              min="0"
-              max="1000"
-              step="10"
-              onChange={(e) => rangeHandle(e)}
-            />
-          </div>
-          <div onClick={(e) => closeFilterBar()} className="cursor-pointer">
-            <svg
-              className="w-8 h-8 text-gray-700 hover:bg-gray-200 rounded-full p-1"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M6 18L18 6M6 6l12 12"
+      <div className="w-full flex flex-row">
+        <div className="w-2/3">
+          <div className="font-medium py-2">Filter by price</div>
+          <div className="flex justify-between items-center">
+            <div className="flex flex-col space-y-2  w-2/3 lg:w-2/4">
+              <label htmlFor="points" className="text-sm">
+                Price (between 0 and 10$):{" "}
+                <span className="font-semibold text-yellow-700">
+                  {range}.00$
+                </span>{" "}
+              </label>
+              <input
+                value={range}
+                className="slider"
+                type="range"
+                id="points"
+                min="0"
+                max="1000"
+                step="10"
+                onChange={(e) => rangeHandle(e)}
               />
-            </svg>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="w-full flex flex-col">
-        <div className="font-medium py-2">Filter by Make Model</div>
-        <div className="flex justify-between items-center">
-          <div className="flex flex-col space-y-2  w-2/3 lg:w-2/4">
-            <label htmlFor="points" className="text-sm">
-              Select Make Model
-            </label>
-            {/* WILL REPLACE MAKE MODEL HERE */}
-            <MultiSelect
-              options={pCategories || []}
-              value={[]}
-              onChange={() => {}}
-              hasSelectAll={false}
-              labelledBy={""}
-            />
-          </div>
-          <div onClick={(e) => closeFilterBar()} className="cursor-pointer">
-            <svg
-              className="w-8 h-8 text-gray-700 hover:bg-gray-200 rounded-full p-1"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M6 18L18 6M6 6l12 12"
+        <div className="w-2/3 mx-2">
+          <div className="font-medium py-2">Filter by Make Model</div>
+          <div className="flex justify-between items-center">
+            <div className="flex flex-col space-y-2  w-2/3 lg:w-2/4">
+              <label htmlFor="points" className="text-sm">
+                Select Make Model
+              </label>
+              {/* WILL REPLACE MAKE MODEL HERE */}
+              <MultiSelect
+                options={pCategories || []}
+                value={[]}
+                onChange={() => {}}
+                hasSelectAll={false}
+                labelledBy={""}
               />
-            </svg>
+            </div>
+            <div onClick={(e) => closeFilterBar()} className="cursor-pointer">
+              <svg
+                className="w-8 h-8 text-gray-700 hover:bg-gray-200 rounded-full p-1"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </div>
           </div>
         </div>
       </div>
